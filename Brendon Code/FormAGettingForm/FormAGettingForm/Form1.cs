71b7f059515cecbed8e5c81ead0c89c7479b85e5
Brendon Blau <brendon.blau@yahoo.com>
2019-04-15 17:47:18 -0700

    Finding Save File
    This is a way to find the save file depending where we put it.

---
+++ Brendon Code/FormAGettingForm/FormAGettingForm/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace FormAGettingForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadChara_Click(object sender, EventArgs e)
+        {
+            //FileInfo fi = new FileInfo(Application.StartupPath);
+            DirectoryInfo fiParent = Directory.GetParent(Application.StartupPath);
+            FileInfo[] files = fiParent.GetFiles();
+            foreach(FileInfo f in files)
+            {
+                //if condition to grab the save file.
+                if(f.Extension == ".resx")
+                {
+                    lbSavedFiles.Items.Add(f.FullName);
+                }
+                
+            }
+            //lbSavedFiles.Items.Add();
+
+
+           
+        }
+    }
+}
