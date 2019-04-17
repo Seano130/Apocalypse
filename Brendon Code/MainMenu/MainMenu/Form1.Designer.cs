8f4c363af3c4362204b0fe1055acf540b0e746c9
Brendon Blau <brendon.blau@yahoo.com>
2019-04-17 11:58:47 -0700

    example of how to handle main menu
    Working on entering in information for the character for new game.

---
+++ Brendon Code/MainMenu/MainMenu/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace MainMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWelcomeScreen = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnLoadGame = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblWelcomeScreen
+            // 
+            this.lblWelcomeScreen.AutoSize = true;
+            this.lblWelcomeScreen.Location = new System.Drawing.Point(306, 61);
+            this.lblWelcomeScreen.Name = "lblWelcomeScreen";
+            this.lblWelcomeScreen.Size = new System.Drawing.Size(83, 13);
+            this.lblWelcomeScreen.TabIndex = 0;
+            this.lblWelcomeScreen.Text = "Welcom Screen";
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(284, 176);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(148, 23);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnLoadGame
+            // 
+            this.btnLoadGame.Location = new System.Drawing.Point(284, 219);
+            this.btnLoadGame.Name = "btnLoadGame";
+            this.btnLoadGame.Size = new System.Drawing.Size(147, 23);
+            this.btnLoadGame.TabIndex = 2;
+            this.btnLoadGame.Text = "LoadGame";
+            this.btnLoadGame.UseVisualStyleBackColor = true;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(284, 298);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(147, 23);
+            this.btnQuit.TabIndex = 3;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 568);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnLoadGame);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lblWelcomeScreen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWelcomeScreen;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnLoadGame;
+        private System.Windows.Forms.Button btnQuit;
+    }
+}
+
---
+++ Brendon Code/MainMenu/MainMenu/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainMenu
+{
+    public partial class Form1 : Form
+    {
+
+      
+        private bool __quitBool = true;
+        List<Label> labelsAdded = new List<Label>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            lblWelcomeScreen.Text = "Character Creation";
+            btnLoadGame.Hide();
+            btnNewGame.Hide();   
+            SetupNewGameLayout();
+            __quitBool = !__quitBool;
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            __quitBool = !__quitBool;
+            if (__quitBool)
+            {
+                DeleteAddedControls();
+                lblWelcomeScreen.Text = "Welcome Screen";
+                btnLoadGame.Show();
+                btnNewGame.Show();
+                
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+        private void SetupNewGameLayout()
+        {
+            Label lb = new Label();
+            lb.Text = "Character Name:";
+            lb.Width = 100;
+            lb.Height = 20;
+            lb.Location = new Point(50, 100);
+            labelsAdded.Add(lb);
+            this.Controls.Add(lb);
+            lb = new Label();
+            lb.Text = "Character Class:";
+            lb.Width = 100;
+            lb.Height = 20;
+            lb.Location = new Point(50, 120);
+            labelsAdded.Add(lb);
+            this.Controls.Add(lb);
+        }
+        private void DeleteAddedControls()
+        {
+            foreach(Label l in labelsAdded)
+            {
+                this.Controls.Remove(l);
+            }
+        }
+    }
+}
