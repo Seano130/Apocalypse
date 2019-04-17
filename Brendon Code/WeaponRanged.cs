d5dea55f19bfabb13fcedec29f31b4d4126a9d7a
Brendon Blau <brendon.blau@yahoo.com>
2019-04-17 10:17:03 -0700

    WeaponMelee and WeaponRanged classes
    the start of the weapon melee and weapon range classes, 
    they have a base inheritance from weapon. so upon creation we need to only provide the name and damage.
    This is only for now but in the future when these classes need more information we will include it.

---
+++ Brendon Code/WeaponRanged.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormAGettingForm
+{
+    public class WeaponRanged:Weapon
+    {
+        public WeaponRanged(string weapon_name, float weapon_damage):base(weapon_name, weapon_damage, WeaponType.Ranged)
+        {
+
+        }
+    }
+}
