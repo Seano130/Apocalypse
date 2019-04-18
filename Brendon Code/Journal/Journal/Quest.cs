c786e21147eaef17e05e13bafcdde29e3940d3f7
Brendon Blau <brendon.blau@yahoo.com>
2019-04-17 20:21:33 -0700

    Journal
    start of journal ideas

---
+++ Brendon Code/Journal/Journal/Quest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journal
+{
+    public class Quest
+    {
+        private string questName, questDiscription;
+        private bool completed;
+
+
+        public string GetQuestName{ get{ return questName; } }
+        public string GetQuestDescription { get{ return questDiscription; } }
+        public bool GetCompleted { get{ return completed; } }
+
+        public Quest(string questName, string questDiscription, bool completed)
+        {
+            this.questName = questName;
+            this.questDiscription = questDiscription;
+            this.completed = completed;
+        }
+
+    }
+}
---
+++ Brendon Code/Journal/Journal/frmJournal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Journal
+{
+    public partial class frmJournal : Form
+    {
+
+        protected List<Quest> quests = new List<Quest>();
+
+        public frmJournal()
+        {
+            InitializeComponent();
+            CreateInfo();
+            DisplayInfo();
+   
+        }
+        protected void DisplayInfo()
+        {
+            lblStatus.Text = "";
+            lblCurrentQuest.Text = quests[1].GetQuestName;
+            foreach (Quest q in quests)
+            {
+                lblStatus.Text += "\n"+q.GetQuestDescription;
+            }
+        }
+        protected void CreateInfo()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Quest q = new Quest("Main Quest", "This is the main quest discription", false);
+                quests.Add(q);
+            }
+        }
+
+    }
+}
