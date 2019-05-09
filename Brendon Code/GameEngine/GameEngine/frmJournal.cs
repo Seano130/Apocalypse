df5b6c1271b2283a4b86ff4126096d4450d3ce28
Brendon Blau <brendon.blau@yahoo.com>
2019-05-08 18:55:52 -0700

    some game engine stuff
    worked on a way to handle the character running around for first time. use "aswd" keys and "j" key to look at journal.
    I am working on a delegate to handle the un-pause after exiting the journal.

---
+++ Brendon Code/GameEngine/GameEngine/frmJournal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//https://colinmackay.scot/2005/04/22/passing-values-between-forms-in-net/
+
+namespace GameEngine
+{
+    public partial class frmJournal : Form
+	{
+		
+
+		protected List<Quest> quests = new List<Quest>();
+		
+		
+
+		public frmJournal(Quest quest)
+        {
+            InitializeComponent();
+            CreateInfo(quest);
+            DisplayInfo();
+			this.FormClosing += FrmJournal_FormClosing;
+			
+        }
+
+		private void FrmJournal_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			MessageBox.Show("Closed Quests");
+		}
+
+		protected void DisplayInfo()
+        {
+            lblStatus.Text = "";
+			foreach(Quest n in quests)
+			{
+				lblCurrentQuest.Text = n.GetQuestName;
+			}
+            //lblCurrentQuest.Text = quests[1].GetQuestName;
+            foreach (Quest q in quests)
+            {
+                lblStatus.Text += "\n"+q.GetQuestDescription;
+            }
+        }
+        protected void CreateInfo(Quest quest)
+        {
+           
+			quests.Add(quest);
+           
+        }
+		
+    }
+}
